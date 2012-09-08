7a645de1766048b3d2af7182c747c6337863a4e4
Vlad Barosan <vlad.barosan@gmail.com>
2012-09-08 10:41:23 +0300

    Added main page template. Test static page. Added .gitignore for pyc files

--- cloudywebproject.py
+++ cloudywebproject.py
@@ -1,9 +1,14 @@
 import webapp2
+import jinja2
 
-
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader('./templates/') )
 class MainPageHandler(webapp2.RequestHandler):
+    """ Handles the main page proccesing"""
+
     def get(self):
-        self.response.out.write('Hello World!')
+        
+        mainPage = jinja_env.get_template("mainpage.html")
+        self.response.out.write(mainPage.render())
 
 
 app = webapp2.WSGIApplication( [('/', MainPageHandler)]
