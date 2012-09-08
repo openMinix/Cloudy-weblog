90da7d6c9175159b381d5e525439beaedc03f112
Vlad Barosan <vlad.barosan@gmail.com>
2012-09-08 16:22:05 +0300

    Added blog front page and created handler for it

--- cloudywebproject.py
+++ cloudywebproject.py
@@ -2,6 +2,8 @@ import webapp2
 import jinja2
 
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader('./templates/') )
+
+
 class MainPageHandler(webapp2.RequestHandler):
     """ Handles the main page proccesing"""
 
@@ -11,7 +13,18 @@ class MainPageHandler(webapp2.RequestHandler):
         self.response.out.write(mainPage.render())
 
 
-app = webapp2.WSGIApplication( [('/', MainPageHandler)]
+class BlogHandler(webapp2.RequestHandler):
+    """Handles the blog page proccesing"""
+
+    def get(self):
+        blogPage = jinja_env.get_template("blog.html")
+        self.response.out.write(blogPage.render())
+
+  
+  
+app = webapp2.WSGIApplication( [('/', MainPageHandler),
+                                ('/blog/?',BlogHandler),
+                              ]
                               ,
                               debug=True)
 
